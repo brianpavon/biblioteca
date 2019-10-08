d3bb43815f53ec408fd9d007e5a0c10575f7f132
brianpavon <53911453+brianpavon@users.noreply.github.com>
2019-10-08 00:32:56 -0300

    Agrego validaciones

---
+++ Biblioteca fantasma/validaciones.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX_INPUT_BUFFER 4096
+
+
+int validarInt(char* buffer);
+int validarString(char* buffer);
+int esNumeroTelefonico(char *buffer);
+
+
