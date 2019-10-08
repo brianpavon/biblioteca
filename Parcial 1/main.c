423a26b47eed4423f6a24f93b0c14d07ed595521
alumno <alumno@LAB00PC00>
2019-10-08 20:30:57 -0300

    agregue algunos maximos

---
+++ Parcial 1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+#include "entidad.h"
+#include "validaciones.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ Parcial 1/validaciones.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX_INPUT_BUFFER 4096
+
+
+int validarInt(char *buffer);
+int validarStringLetras(char *buffer);
+int esNumeroTelefonico(char *buffer);
+int esFloat(char *buffer);
+
+
